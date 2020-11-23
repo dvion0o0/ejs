6b3715363c35906631443cfde961937336b3435e
Dv <dv@Dvs-MacBook-Pro.local>
2020-11-23 14:25:44 +0530

    new changes

--- app.js
+++ app.js
@@ -3,43 +3,33 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var items = ['Buy Food', 'Cook Food', 'Eat Food'];
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) =>{
 
 var today = new Date();
-var currentDay = today.getDay();
-var day = "";
+var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+};
 
-switch (currentDay) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day ="saturday";
-        break;
-    default:
-        console.log(`Error: current day is equal to ${currentDay}`);
-        break;
-}
+var day = today.toLocaleDateString('en-US', options);
 
-res.render('list', {day: "Monday"});
+res.render('list', {kindofday: day, newListItems: items});
 });
+
+
+app.post("/", (req,res) =>{
+    var item = req.body.newitem;
+    items.push(item);
+    res.redirect("/");
+})
+
+
 
 app.listen(8000, ()=> console.log('sevrer has started on port 8000'));
