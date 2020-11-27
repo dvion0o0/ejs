28cbf0d98a398c62a0ea871c72e36f7ee3b3eb3a
Dv <dv@Dvs-MacBook-Pro.local>
2020-11-28 00:42:18 +0530

    added database

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-var items = ['Buy Food', 'Cook Food', 'Eat Food'];
+const url = "mongodb://localhost:27017/todolistDB";
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -11,26 +12,77 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
+mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology:true});
+
+const schema = new mongoose.Schema({
+    name:String
+});
+
+const list = mongoose.model("List", schema);
+
+const list1 = new list ({
+    name:"Welcome to your todo list!"
+});
+
+const list2 = new list ({
+    name:"Hit the + button to add a new list."
+});
+
+const list3 = new list ({
+    name:"hit checkbox to delete the list."
+});
+
+const defaultItems = [list1, list2, list3];
+
+
+
 app.get("/", (req, res) =>{
-
-var today = new Date();
-var options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-};
-
-var day = today.toLocaleDateString('en-US', options);
-
-res.render('list', {kindofday: day, newListItems: items});
+list.find({}, (err, foundItems) =>{
+if(foundItems.length === 0){
+list.insertMany(defaultItems, (err) =>{
+    if(err) throw err;
+    console.log("succesfully saved to the collection");
+    res.redirect("/");
+});
+}else{
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+}
+});
 });
 
 
 app.post("/", (req,res) =>{
-    var item = req.body.newitem;
-    items.push(item);
-    res.redirect("/");
-})
+  const listName = req.body.newitem;
+
+  console.log(listName);
+
+  const item = new list({
+      name: listName
+  });
+
+item.save();
+
+res.redirect("/");
+
+});
+
+
+app.post("/delete", (req,res) =>{
+   const checkedId = req.body.checkbox; 
+list.findOneAndDelete(checkedId, (err) =>{
+    if(err) throw err;
+    console.log("item deleted");
+    res.redirect("/")
+});
+
+});
+
+
+// app.get("/work", (req, res) =>{
+//     res.render("list", {listTitle: 'Work List', newListItems: workItems});
+// });
+
+
 
 
 
